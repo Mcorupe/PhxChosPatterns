bf3f7cb8780cec2ed3a7c17800f6844683d839b0
Kevin Griego <Kevin.Greigo@aexp.com>
2019-10-04 14:13:48 -0700

    updated to show pattern details

--- src/components/patterns/PatternDetails.js
+++ src/components/patterns/PatternDetails.js
@@ -10,19 +10,42 @@ const PatternDetails = props => {
   const { pattern, auth } = props;
   if (!auth.uid) return <Redirect to="/signin" />;
   if (pattern) {
+    //if im destructuring pattern off props... why do i have to use pattern.___
     return (
-      //if im destructuring pattern off props... why do i have to use pattern.___
-      <div className="container section pattern-details">
-        <div className="card z-depth-0">
-          <div className="card-content">
-            <span className="card-title">{pattern.title}</span>
-            <p>{pattern.content}</p>
-            <p>These are PatternDetails</p>
-          </div>
-          <div className="card-action grey lighten-4 grey-text">
-            <div>
-              Posted by {pattern.authorFirstName}
-              {pattern.authorLastName}
+      <div>
+        <div className="container section pattern-details">
+          <div className="card z-depth-0">
+            <div className="card-content">
+              <span className="card-title">{pattern.title}</span>
+              <div>
+                {pattern.content.map((obj, idx) => {
+                  return (
+                    <div key={`${pattern.authorId}-${idx}`}>
+                      {Object.keys(obj.feet).map(key => (
+                        <div key={`${pattern.authorID}-${key}`}>
+                          {key}:{obj.feet[key]}
+                        </div>
+                      ))}
+                      {Object.keys(obj.hands).map(key => {
+                        const data = obj.hands[key];
+                        return (
+                          <div key={`${pattern.authorID}-${key}`}>
+                            {key}:{data}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+              <p>These are PatternDetails</p>
+            </div>
+            <div className="card-action grey lighten-4 grey-text">
+              <div>
+                Posted by {pattern.authorFirstName}
+                {pattern.authorLastName}
+              </div>
+              {moment(pattern.createdAt.toDate()).format("MMMM Do YYYY, h:mm")}
             </div>
             {moment(pattern.createdAt.toDate()).format("MMMM Do YYYY, h:mm")}
           </div>
